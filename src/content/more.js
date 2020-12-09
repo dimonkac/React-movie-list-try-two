4854bd2f7f901be435830d2cbb86b86b2e937048
dimonkac <61842792+dimonkac@users.noreply.github.com>
2020-12-09 15:11:08 +0200

    stylecommit

--- src/content/more.js
+++ src/content/more.js
@@ -1,50 +1,9 @@
-// import React, { useState, useEffect } from "react";
-// // import { ReactComponent as Icon } from "../img/like.svg";
-// import Api from "../helpersAPI";
-// // import { Link } from "react-router-dom";
-
-// import "./page.css";
-
-// export const More = ({
-//   match: {
-//     params: { id },
-//   },
-// }) => {
-//   const [detail, setDetail] = useState([]);
-
-//   const getMoviesMore = async () => {
-//     const info = await Api.getMoviesMore(id).catch(console.log);
-//     if (info) setDetail(info);
-//     console.log(info);
-//   };
-
-//   useEffect(() => {
-//     getMoviesMore();
-
-//     console.log(detail);
-//   }, []);
-
-//   const renderMovie = () => {
-//     return detail.map(({ poster_path, release_date, title, id }) => (
-//       <div key={id} className="cart">
-//         <div className="poster">
-//           <img src={Api.poster_url + poster_path} alt={title} />
-//         </div>
-//         <div className="poster">{release_date}</div>
-//         <div className="poster">{title}</div>
-//       </div>
-//     ));
-//   };
-
-//   return <div className="wraper"> {renderMovie} </div>;
-// };
-
 import React, { useState, useEffect } from "react";
-// import { ReactComponent as Icon } from "../img/like.svg";
+
 import Api from "../helpersAPI";
-// import { Link } from "react-router-dom";
 
 import "./page.css";
+import "./more.css";
 
 export const More = ({
   match: {
@@ -74,22 +33,30 @@ export const More = ({
       backdrop_path,
     } = detail;
     return (
-      <div
-        style={{
-          backgroundImage: `url(${Api.poster_url + backdrop_path})`,
-          backgroundSize: "cover",
-        }}
+      <section
+        className="containerFilm"
+        // style={{
+        //   backgroundImage: `url(${Api.poster_url + backdrop_path})`,
+        // }}
       >
-        <h3>{original_title}</h3>
-        <p>{overview}</p>
-        <h3>Genres</h3>
-        <p>
-          {genres.map(({ name }) => (
-            <div key={name}>{name}</div>
-          ))}
-        </p>
-        <img src={`${Api.poster_url + poster_path}`} />
-      </div>
+        <img
+          className="backgroundFilm"
+          src={`${Api.poster_url + backdrop_path}`}
+        />
+        <div>
+          <img src={`${Api.poster_url + poster_path}`} />
+        </div>
+        <div>
+          <h3>{original_title}</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <p>
+            {genres.map(({ name }) => (
+              <div key={name}>{name}</div>
+            ))}
+          </p>
+        </div>
+      </section>
     );
   }
 };
--- src/wraper/styled.js
+++ src/wraper/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wraper = styled.section`
   width: 100%;
-  background-color: #ece898;
+  background-color: #676766;
   min-height: calc(100vh - 80px);
   padding: 30px;
 `;
